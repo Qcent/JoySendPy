7109839c3c7ade50fa2b6a040381fe1c96bb01e5
Qcent <dquinn8@cogeco.ca>
2024-08-17 13:28:34 -0400

    updated JoySender.py to more closely mirror JoySender++

--- JoySender.py
+++ JoySender.py
@@ -1,13 +1,17 @@
-import hid
-import keyboard
+import time
+
 import pygame
 import socket
 import ipaddress
 import argparse
 import sys
+
 from utils.helper_functions import *
 from utils.gamepad_mapping import *
 from utils.xbox_reports import XBOX_REPORT
+
+import colorama
+colorama.init()
 
 RESTART = 'shift+R'
 QUIT = 'shift+Q'
@@ -101,21 +105,29 @@ def select_device(op_mode, auto_select):
     return gamepad, op_mode, vendor_id, product_id
 
 
-def flush_HID_buffer(gamepad, clock):
+def flush_HID_buffer(gamepad, clock, ds4_data_offset):
     # clear the read buffer of any unread values
-    # this is important so that we don't read old values from previous requests sent to the device
+    # this is important so that we don't read old values from the device
 
     # hidapi has no flush so this function acts as the frame limiter
     # while reading reports to keep them as current as possible
-    now = start_time = time.monotonic()
-    elapsed_time = start_time - clock.last_frame_time
+    current_time = time.monotonic()
+    elapsed_time = current_time - clock.last_frame_time
     sleep_time = clock.target_frame_time - elapsed_time
 
-    while (now - start_time) < sleep_time:
-        data = gamepad.read(64);
-        now = time.monotonic()
+    if ds4_data_offset == 3:
+        if sleep_time > 0:  # works well when connected via BT but causes input delay when connected via USB
+            pygame.time.wait(int(sleep_time - DS4_REPORTING_DELAY))
+            data = gamepad.read(64)
+            pygame.time.wait(int(DS4_REPORTING_DELAY))
+    else:
+        # produces inaccurate (higher) frame rate but no input delay on USB
+        for x in range(int(sleep_time / DS4_REPORTING_DELAY)-1):
+            data = gamepad.read(64)
+            pygame.time.wait(int(DS4_REPORTING_DELAY))
 
-    clock.last_frame_time = start_time
+    clock.last_frame_time = current_time
+    clock.frame_count += 1
 
 
 def get_host_address():
@@ -153,11 +165,24 @@ def establish_connection(ip_address, op_mode):
     return client_socket
 
 
+def activate_ds4_extended_reports(gamepad, ds4_data_offset):
+    if ds4_data_offset == 1:
+        # READ USB CALIBRATE REPORT 0x02 // size is 37 bytes
+        gamepad.get_feature_report(0x02, 37)
+        return 1
+    elif ds4_data_offset == 3:
+        # READ BT CALIBRATE REPORT 0x05 // size is 41 bytes
+        gamepad.get_feature_report(0x05, 41)
+        return 1
+    return 0
+
+
 def joySender(operational_mode, auto_select):
     clock = FpsLimiter(TARGET_FPS)
     failed_connections = 0
-    end_time = 0
+    sleep_time = 0
     start_time = 0
+    last_output = time.monotonic()
     vendor_id = product_id = None
     buttons = None
     map_name = None
@@ -176,16 +201,33 @@ def joySender(operational_mode, auto_select):
         map_name = f'{hex(vendor_id)}{hex(product_id)}'
     elif operational_mode == 2:
         print(f'DS4 Full Motion Mode Activated')
-        # activate extended ds4 reports
-        ##gamepad.write()
         # first byte is used to determine where stick input starts
         ds4_data_offset = 3 if gamepad.read(64)[0] == 0x11 else 1
-        # set lightbar color values
-        ds4_output_report[8] = 105  # R
-        ds4_output_report[9] = 4    # G
-        ds4_output_report[10] = 32  # B
 
-        gamepad.write(ds4_output_report)
+        # activate extended ds4 reports
+        result = activate_ds4_extended_reports(gamepad, ds4_data_offset)
+
+        # cant seem to get the writing of output reports to work through python
+        '''
+        # set lightbar color values
+        ds4_output_report[8] = 0  # R
+        ds4_output_report[9] = 4    # G
+        ds4_output_report[10] = 255  # B
+
+        # Copy the contents of array2 to array1 starting from index 3
+        # ds4_output_report[3:3+len(ds4_bt_state_data)] = ds4_bt_state_data
+
+        byte_array = [
+            0x11, 0xc0, 0x20, 0xf3, 0x04, 0x00, 0x00, 0x00, 0x00, 0x04, 0xff, 0x00, 0x00, 0x00, 0x00, 0xfb,
+            0x7f, 0x00, 0x00, 0xd0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0xf5, 0xd9, 0x13, 0xae,
+            0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x5b, 0x05, 0x3f, 0xbc, 0xfb,
+            0x7f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x43, 0x4b, 0x43, 0x48, 0xf6, 0x7f,
+            0x00, 0x00, 0xd0, 0x0a, 0x45, 0x48, 0xf6, 0x7f, 0x00, 0x00, 0xb0, 0xf5, 0x00, 0x00, 0x00, 0x03,
+        ]
+
+        result = gamepad.write(byte_array)
+        print(result)
+        '''
     else:
         print(f'PyGame Mode Activated')
         buttons = PyGameButtonMapping()
@@ -250,7 +292,8 @@ def joySender(operational_mode, auto_select):
             ###################################
             # let's calculate some latency
             if args.latency and loop_count == 19:
-                start_time = time.time()
+                #start_time = time.time()
+                start_time = time.monotonic()
 
             ###################################
             # Send joystick input to server
@@ -268,16 +311,22 @@ def joySender(operational_mode, auto_select):
             # Wait for server response
             try:
                 response = client_socket.recv(1024)
-                if args.latency:
-                    loop_count += 1
-                    if loop_count == 20:
-                        end_time = time.time()  # 20 round trips completed
-                        print(f'Latency: {((end_time - start_time) * 1000) - 1000 / TARGET_FPS : .2f} ms')
-                        loop_count = 0
             except Exception:
-                print("<< Connection Lost >>")
+                print(f"<< Connection Lost >>")
                 client_socket.close()
                 break
+
+            if args.latency:
+                loop_count += 1
+                if loop_count == 20:
+                    end_time = time.monotonic()  # 20 round trips completed
+                    test = (end_time - last_output)
+                    print(f"fps:{(clock.frame_count/(end_time - last_output)): .2f} \tLatency: {(end_time - start_time):.5f} ms")
+                    # Move the cursor up by 1 line
+                    print("\033[1A", end="")
+                    loop_count = 0
+                    clock.frame_count = 0
+                    last_output = end_time
 
 
             ##################################
@@ -299,14 +348,14 @@ def joySender(operational_mode, auto_select):
             # set clock to limit FPS
             if operational_mode == 2:
                 # flush input buffer for up-to-date reports
-                flush_HID_buffer(gamepad, clock)
+                flush_HID_buffer(gamepad, clock, ds4_data_offset)
             else:
                 clock.tick()
 
         # Shift+R will reset program allowing joystick reconnection/selection, holding a number will change op mode
         if keyboard.is_pressed(RESTART):
             while keyboard.is_pressed(RESTART):
-                True
+                pass
             if keyboard.is_pressed('1'):
                 return 2
             if keyboard.is_pressed('2'):
